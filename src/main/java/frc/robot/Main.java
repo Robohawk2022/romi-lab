2280305453dbae16624660b65adbbb3b10a3d7fa
crwright77 <98225554+crwright77@users.noreply.github.com>
2023-01-28 13:45:59 -0500

    Python Vision Added

--- src/main/java/frc/robot/Main.java
+++ src/main/java/frc/robot/Main.java
@@ -14,9 +14,9 @@ import frc.robot.examples.TankDrivingRobot;
 public final class Main {
 
     public static void main(String... args) {
-        // RobotBase.startRobot(TripleModeDrivingRobot::new);
+        RobotBase.startRobot(TripleModeDrivingRobot::new);
         // RobotBase.startRobot(PadDrivingRobot::new);
-        RobotBase.startRobot(PIDPositionControlledRobot::new);
+        // RobotBase.startRobot(PIDPositionControlledRobot::new);
         // RobotBase.startRobot(PIDExperimentationRobot::new);
         // RobotBase.startRobot(PIDTurningControlledRobot::new);
         // RobotBase.startRobot(OnOffRobot::new);
